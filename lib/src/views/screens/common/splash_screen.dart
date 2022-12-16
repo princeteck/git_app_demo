1a18dd2cb1b1f9ad144112c062abdbc22ec1a265
Yuvraj Sonawane <privateyuvraj@gmail.com>
2022-12-16 19:13:55 +0530

    splash screen updated

--- lib/src/views/screens/common/splash_screen.dart
+++ lib/src/views/screens/common/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:git_app/src/constants/system/app_status.dart';
 import 'package:git_app/src/constants/system/pref_constants.dart';
+import 'package:git_app/src/constants/ui/app_icons_constants.dart';
 import 'package:git_app/src/core/di/app_init.dart';
 import 'package:git_app/src/helpers/shared_preferences.dart';
 import 'package:git_app/src/views/screens/auth/login_screen.dart';
@@ -53,16 +55,37 @@ class _SplashScreenState extends State<SplashScreen> {
       builder: (context, state) {
         return Scaffold(
           body: SafeArea(
-            child: SizedBox(
+            child: Container(
               width: context.widthPx,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: context.brightness == Brightness.light
+                      ? [const Color(0XFF1E3B70), const Color(0XFF29539B)]
+                      : [const Color(0XFF28313B), const Color(0XFF485461)],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
               child: Column(
                 mainAxisSize: MainAxisSize.max,
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: const [
-                  Text('Splash Screen'),
-                  SizedBox(height: 20),
-                  CircularProgressIndicator(),
+                children: [
+                  const Spacer(),
+                  Center(
+                    child: SvgPicture.asset(
+                      AppIcons.fork,
+                      height: context.heightPx * 0.4,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Spacer(),
+                  Text(
+                    'Splash Screen',
+                    style: context.theme.textTheme.bodyLarge?.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20),
                 ],
               ),
             ),
